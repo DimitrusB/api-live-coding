6a3e5dee378d1988af3af2b3ca4b148cd220a635
DimitrusB <dimitrusboss@mail.ru>
2023-05-30 23:43:58 +0300

    чистка

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 import { delTodos, getTodos, postTodos } from "./api.js";
 
-
-const host = 'https://webdev-hw-api.vercel.app/api/v2/todos';
 let tasks = [];
 let token = "Bearer bgc0b8awbwas6g5g5k5o5s5w606g37w3cc3bo3b83k39s3co3c83c03ck";
 token = null;
